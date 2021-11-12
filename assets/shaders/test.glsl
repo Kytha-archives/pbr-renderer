bae0d8ec2ce4fc474726b60d998facf115c30377
Kyle Thatcher <thatchek@mcmaster.ca>
2021-11-11 20:35:24 -0500

    Decrement shader openGl version

--- assets/shaders/test.glsl
+++ assets/shaders/test.glsl
@@ -1,5 +1,5 @@
 #type vertex
-#version 330 core
+#version 300 core
 layout (location = 0) in vec3 a_Position;
 layout (location = 1) in vec3 a_Normal;
 layout (location = 2) in vec3 a_Tangent;
@@ -16,7 +16,7 @@ void main()
 }
 
 #type fragment
-#version 330 core
+#version 300 core
 out vec4 FragColor;
 
 struct DirLight {
